7ddb535d9093a3bfe6726c58b7f06b417fb027f6
duoduola0717 <853988880@139.com>
2019-08-21 10:27:04 +0800

    Initial commit

--- resources/views/sun.blade.php
+++ resources/views/sun.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sun
+                    Sun11
                 </div>
 
                 <div class="links">
